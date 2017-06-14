cc235a662b7667185b3706c91c2dff654c69c820
Jacob Ayers <ayerswebdev@gmail.com>
2017-06-14 12:10:37 -0500

    hid ads for yearbook and credit card on mobile, replacing them with links in the quick links menu; the quick links menu is a dropdown on mobileand defaults as hidden; added some mobile-only drop-down arrows to the quick links menu

--- js/hhs.js
+++ js/hhs.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
   $("#year").html(new Date().getFullYear());
+
+  $("#ql-btn").click(function() {
+    if(!window.matchMedia("(min-width: 1024px)").matches) {
+      $("#ql-dropdown").slideToggle(400);
+      setTimeout(function() {
+        $("#ql-arrow").toggleClass("fa-caret-down");
+        $("#ql-arrow").toggleClass("fa-caret-up");
+      }, 400)
+    }
+  });
 });
